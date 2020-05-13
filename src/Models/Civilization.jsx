d5d686456c0e46adcb56f84a48ca759bc5f6dc27
JonLucasWard <jonlucasward@gmail.com>
2020-05-13 00:02:11 -0400

    CSS works now

--- src/Models/Civilization.jsx
+++ src/Models/Civilization.jsx
@@ -1,89 +1,222 @@
 import * as React from 'react';
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from '@material-ui/core';
 
 export class Civilization extends React.Component {
-    constructor(props){
-        super(props);
-        this.civilization = props.civilization;
-        this.number = props.number;
-        //this.props = {
-            // number: null,
-            // civilization: {
-            //     GovtType: [],
-            //     GovtTypeExplanation: [], 
-            //     Biome: {
-            //         PopulationDistribution: null,
-            //         Biomes: [],
-            //         BiomesExplanations:[]
-            //     },
-            //     AreaControl:null, AreaControlExplanation: null,
-            //     PopulationTrait:null,
-            //     MajoritiesAndMinorities:null, MnMList:[],
-            //     TransportationInfrastructure:null,
-            //     EnergyInfrastructure:null, EnergyInfrastructureExplanation:null,
-            //     GovtReligiousRelations:null, GovtReligiousRelationsExplanation:null,
-            //     EconomicFocus:null, EconomicFocusExplanation:null,
-            //     GenderRelations:null, GenderRelationsExplanation:null,
-            //     MagicOrTechRelations:null, MagicOrTechRelationsExplanation:null,
-            //     ForeignRelations:null, ForeignRelationsExplanation:null,
-            //     MilitarySpecialty:null, MilitarySpecialtyExplanation:null,
-            //     FrequentIssue:null, FrequentIssueExplanation:null,
-            //     PopularIssue:null, MajorCulturalValue:null, MajorCulturalTaboo:null, MajorCulturalIssue:null,
-            //     Literacy:null, PowerScale: null
-
-            // }
-        //}
-    }
-    start(e){
-        let holdMe = Object.keys(this.props.civilization[e]).map(key =>
-            <li key = {key}>{this.props.civilization[e][key]}</li>
-        );
-
-        return holdMe;
+    constructor(props){ //inherit properties of parent, ideally this should be the Setting component
+        super(props); //apply the properties of the parent component
+        this.civilization = props.civilization; //assign a unique prop value of civilization to props.civilization (directly passed in when created)
+        this.number = Number(props.number)+1; //likewise, a number property, props must be forced to be treated as a number to count correctly according to humans
     }
     startB(){
-        let holdMe = Object.keys(this.props.civilization.Biome.Biomes).map(key =>
-            <li key = {key}>{this.props.civilization.Biome.Biomes[key]}</li>
+        let holdMe = Object.keys(this.props.civilization.Biome.Biomes).map(key => //as per above but for Biome sub-object specifically
+            <TableRow><TableCell className="ObjVal" key = {key}>
+                <p className="Tooltip">{this.props.civilization.Biome.Biomes[key]}
+                <span className="Tooltiptext">{this.props.civilization.Biome.BiomesExplanations[key]}</span>
+                </p></TableCell></TableRow>
         );
         
         return holdMe;
     }
+    startM(e){
+        let holdMe = Object.keys(this.props.civilization[e]).map(key => //iterate over the keys of a given type and create a list
+            <TableRow><TableCell className="ObjVal" key = {key}><textarea className="FillMe">{this.props.civilization[e][key]}</textarea></TableCell></TableRow> //civilization object -> key -> the value is an array, what value of array we are on
+        );
 
+        return holdMe; //return the JSX and render it
+    }
 
     render(){
         return (
-            <div style={{"backgroundColor": "yellow"}}>
-            <p>This is civilization: {this.props.number}</p>
-            <ul>
-                <p>GovtTypes</p>
-                {this.start("GovtType")}
-            </ul>
-            <p>Population Distribution: {this.props.civilization.Biome.PopulationDistribution}</p>
-            <ul>
-                {this.startB()}
-            </ul>
-            <p>Area Control: {this.props.civilization.AreaControl}</p>
-            <p>Population Trait: {this.props.civilization.PopulationTrait}</p>
-            <p>Majorities and Minorities: {this.props.civilization.MajoritiesAndMinorities}</p>
-            <ul>
-                {this.start("MnMList")}
-            </ul>
-            <p>Transportation Infrastructure: {this.props.civilization.TransportationInfrastructure}</p>
-            <p>Energy Infrastructure: {this.props.civilization.EnergyInfrastructure}</p>
-            <p>Religious Relations: {this.props.civilization.GovtReligiousRelations}</p>
-            <p>Economic Focus: {this.props.civilization.EconomicFocus}</p>
-            <p>Gender Relations: {this.props.civilization.GenderRelations}</p>
-            <p>Magic/Tech Policy: {this.props.civilization.MagicOrTechRelations}</p>
-            <p>Foreign Relations: {this.props.civilization.ForeignRelations}</p>
-            <p>Military Specialty: {this.props.civilization.MilitarySpecialty}</p>
-            <p>Frequent Issue: {this.props.civilization.FrequentIssue}</p>
-            <p>Popular Issue: {this.props.civilization.PopularIssue}</p>
-            <p>Major Cultural Value: {this.props.civilization.MajorCulturalValue}</p>
-            <p>Major Cultural Taboo: {this.props.civilization.MajorCulturalTaboo}</p>
-            <p>Major Cultural Issue: {this.props.civilization.MajorCulturalIssue}</p>
-            <p>Literacy % of Population: {this.props.civilization.Literacy}</p>
-            <p>Power Ranking of Nation: #{this.props.civilization.PowerScale}</p>
+            <TableContainer className="ObjectDisplay">
+                <Table style={{border:"solid"}}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell className="ObjectMajor" align="center" colspan={3}>This is civilization: <textarea className="FillMe">{this.number}</textarea>
+                                <p className="Tooltip">&nbsp; which ranks #{this.props.civilization.PowerScale} in this setting.
+                                    <span className="Tooltiptext">Within this setting, this civilization would be considered #rank in terms of power or desireability to live there.</span>
+                                </p>
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+            {/*Most of this is simply the name of the trait followed by the specific value in the property*/}
+                    <TableBody>
+                        <TableRow>
+                            <TableCell colspan={3} align="center">
+                                <p className="Tooltip">Systems of Government
+                                        <span className="Tooltiptext">The main features of how this civilization governs itself, raw facts and bureaucratic day-to-day. Know that not everything
+                                        may be intentional or "as it seems".</span>
+                                    </p>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="ObjKey">
+                                <p className="Tooltip">Type 1: &nbsp;
+                                    <span className="Tooltiptext">The first "type" of organizational principle this government has. IE "Republic", or "Capitalist"</span>
+                                </p>
+                                <p className="Tooltip"> {this.props.civilization.GovtType[0]}
+                                    <span className="Tooltiptext">{this.props.civilization.GovtTypeExplanation[0]}</span>
+                                </p>
+                            </TableCell>
+                            <TableCell className="ObjKey">
+                                <p className="Tooltip">Type 2: &nbsp;
+                                    <span className="Tooltiptext">The second "type" of organizational principle this government has. IE "Republic", or "Matriarchy"</span>
+                                </p>
+                                <p className="Tooltip"> {this.props.civilization.GovtType[1]}
+                                    <span className="Tooltiptext">{this.props.civilization.GovtTypeExplanation[1]}</span>
+                                </p>
+                            </TableCell>
+                            <TableCell className="ObjKey">
+                                <p className="Tooltip">Economic Focus: &nbsp;
+                                    <span className="Tooltiptext">What does this civilization primarily use to trade with other civilizations?</span>
+                                </p>
+                                <p className="Tooltip"> {this.props.civilization.EconomicFocus}
+                                    <span className="Tooltiptext">{this.props.civilization.EconomicFocusExplanation}</span>
+                                </p>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="ObjKey">
+                                <p className="Tooltip">Religious Relations: &nbsp; 
+                                    <span className="Tooltiptext">How does this civilization treat Religions?</span>
+                                </p>
+                                <p className="Tooltip">{this.props.civilization.GovtReligiousRelations}
+                                    <span className="Tooltiptext">{this.props.civilization.GovtReligiousRelationsExplanation}</span>
+                                </p>
+                                </TableCell>
+                            <TableCell className="ObjKey">
+                                <p className="Tooltip">Majorities and Minorities: &nbsp; 
+                                    <span className="Tooltiptext">There is often one group of people that can be numerically counted as the "majority" in a civilization. All other groups would be a "minority". How does the government view these groups?</span>
+                                </p>
+                                <p className = "Tooltip">{this.props.civilization.MajoritiesAndMinorities}
+                                    <span className = "Tooltiptext">{this.props.civilization.MajoritiesAndMinoritiesExplanation}</span>
+                                </p>
+                                </TableCell>
+                                {this.startM("MnMList")}
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="ObjKey">
+                                <p className="Tooltip">Transportation Infrastructure: &nbsp;
+                                    <span className="Tooltiptext">What is this civ's preferred focus in moving goods and people from place to place?</span>
+                                </p>
+                                <p className="Tooltip">{this.props.civilization.TransportationInfrastructure}
+                                    <span className="Tooltiptext">{this.props.civilization.TransportationInfrastructureExplanation}</span>
+                                </p>
+                            </TableCell>
+                            <TableCell className="ObjKey">
+                                <p className="Tooltip">Energy Infrastructure: &nbsp;
+                                    <span className="Tooltiptext">What is this civ's preffered focus in how people get heat and operate tools?</span>
+                                </p>
+                                <p className="Tooltip">{this.props.civilization.EnergyInfrastructure}
+                                    <span className="Tooltiptext">{this.props.civilization.EnergyInfrastructureExplanation}</span>
+                                </p>
+                            </TableCell>
+                            <TableCell className="ObjKey">
+                                <p className="Tooltip">Foreign Relations: &nbsp;
+                                    <span className="Tooltiptext">How does this government generally interact with other governments?</span> 
+                                </p>    
+                                <p className="Tooltip">{this.props.civilization.ForeignRelations}
+                                    <span className="Tooltiptext">{this.props.civilization.ForeignRelationsExplanation}</span>
+                                </p>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="ObjKey">
+                                <p className="Tooltip">Military Specialty: &nbsp;
+                                    <span className="Tooltiptext">How this civilization best defends itself in large scale combat.</span>
+                                </p>
+                                <p className="Tooltip">{this.props.civilization.MilitarySpecialty}
+                                    <span className="Tooltiptext">{this.props.civilization.MilitarySpecialtyExplanation}</span>
+                                </p>
+                            </TableCell>
+                            <TableCell className="ObjKey">
+                                <p className="Tooltip">Frequent Issue: &nbsp;
+                                    <span className="Tooltiptext">What is a reccuring problem this civilization has been dealing with?</span>
+                                </p>
+                                <p className="Tooltip">{this.props.civilization.FrequentIssue}
+                                    <span className="Tooltiptext">{this.props.civilization.FrequentIssueExplanation}</span>
+                                </p>
+                                </TableCell>
+                            <TableCell className="ObjKey">
+                                <p className="Tooltip">Magic/Tech Policy: &nbsp;
+                                    <span className="Tooltiptext">What kind of legal policy is implemented for advancing or using Magic/Technology.</span>
+                                </p>
+                                <p className="Tooltip">{this.props.civilization.MagicOrTechRelations}
+                                    <span className="Tooltiptext">{this.props.civilization.MagicOrTechRelationsExplanation}</span>
+                                </p>
+                                </TableCell>
+                        </TableRow>
 
-            </div>
+                        <TableRow>
+                            <TableCell colspan={3} align="center">
+                                <p className="Tooltip">Area Controlled: &nbsp;
+                                    <span className="Tooltiptext">How much space does this civilization actually control?</span> 
+                                </p>
+                                <p className="Tooltip">{this.props.civilization.AreaControl}
+                                    <span className="Tooltiptext">{this.props.civilization.AreaControlExplanation}</span>
+                                </p>    
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="ObjKey" colspan={2}>
+                                <p className="Tooltip">{this.props.civilization.Biome.PopulationDistribution}
+                                    <span className="Tooltiptext">{this.props.civilization.Biome.PopulationDistributionExplanation}</span>
+                                </p>
+                                </TableCell>
+                            {this.startB()}
+                        </TableRow>
+
+                        <TableRow>
+                            <TableCell colspan={3} align="center">
+                                <p className="Tooltip">Culture
+                                    <span className="Tooltiptext">The overarching culture shared by all members of this civilization.</span></p>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                           <TableCell className="ObjKey">
+                               <p className="Tooltip">Population Trait: &nbsp;
+                                    <span className="Tooltiptext">A "stereotype" applied to all members of this population. Something the average person here tends to exhibit.</span>
+                               </p> 
+                               {this.props.civilization.PopulationTrait}
+                               </TableCell>
+                            <TableCell className="ObjKey">
+                                <p className="Tooltip">Gender Relations: &nbsp; 
+                                <span className="Tooltiptext">How is social influence weighted amongst genders.</span>
+                                </p>
+                                <p className="Tooltip">{this.props.civilization.GenderRelations}
+                                <span className="Tooltiptext">{this.props.civilization.GenderRelationsExplanation}</span>
+                                </p>
+                            </TableCell>
+                            <TableCell className="ObjKey">
+                                <p className="Tooltip">Major Cultural Value: &nbsp;
+                                <span className="Tooltiptext">Something that is generally considered desireable or "good" by the people of this civilization.</span>
+                                </p>
+                                {this.props.civilization.MajorCulturalValue}
+                                </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="ObjKey">
+                                <p className="Tooltip">Popular Issue: &nbsp;
+                                    <span className="Tooltiptext1">Something that the people of this civilization tend to talk about, as a point of belief, concern, controversy, or identity.</span>
+                                </p>
+                                {this.props.civilization.PopularIssue}</TableCell>
+                            <TableCell className="ObjKey">
+                                <p className="Tooltip">Major Cultural Issue: &nbsp;
+                                    <span className="Tooltiptext1">Something that is causing a rift in culture amongst the people, with them "picking sides".</span>
+                                </p>
+                                {this.props.civilization.MajorCulturalIssue}
+                            </TableCell>
+                            <TableCell className="ObjKey">
+                                <p className="Tooltip">Major Cultural Taboo: &nbsp;
+                                    <span className="Tooltiptext1">Something that is forbidden, discouraged, undesireable, or just not discussed according to the culture of this civilization.</span>
+                                </p>
+                                {this.props.civilization.MajorCulturalTaboo}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
         )
     }
 }
--- src/Pages/Setting.jsx
+++ src/Pages/Setting.jsx
@@ -7,53 +7,61 @@ import {Civilization} from '../Models/Civilization';
 const callSetting = "/Setting/";
 
 export class Setting extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
+    constructor(props){ //inherit props from parent component to use in thise one, in this case, App(.js)
+        super(props); //apply those props to this component
+        this.state = { //state, unique characteristics of this component
+            //booleans on whether or not the user chose to open a section of the page, false is default
             planet: false,
             end: false,
             civs: false,
             reli: false,
             towns: false,
-            civilization: []
+            civilization: [], //an array meant to hold civilization objects, will be passed into Civilization model component
+            civsInSetting: 1 //# of civs in setting
 
         };
-        this.toggle = this.toggle.bind(this);
+        //the following functions need to use the state, and are declared here to be able to do that
+        this.toggle = this.toggle.bind(this); //this toggle function will bind the word "this" to the state characteristic of this file, we will not accidentally call the "state" of the function
         this.makeCiv = this.makeCiv.bind(this);
         this.loadData = this.loadData.bind(this);
+        this.handleCivNumber = this.handleCivNumber.bind(this);
     };
 
     //it seems that binding a function to the constructor will prevent it from firing on every re-render
 
-    toggle(e){
-        let x = e.target.value.toLowerCase() === 'true' ? true : false;
-        this.setState({[e.target.id]: !x} );
+    toggle(e){ //for the buttons which trigger the Collapse component, allowing more things to populate and be seen
+        let x = e.target.value.toLowerCase() === 'true' ? true : false; //change the target id string to lower case, if it is equivalent to the word "true", then assign it boolean true, else, false
+        this.setState({[e.target.id]: !x} ); //apply the state key (should be the same as the object id) to the boolean given in x
     }
 
     arrow(key){ //accept string value that correlates to a state key
-        if(this.state[key]){ //passing in the key value to the state should return one of the 
+        if(this.state[key]){ //passing in the key value to the state should return a boolean value, if true, the Collapse is open and the symbol should change accordingly, else, it is closed
             return <ExpandMore/>
         } else {
             return <NavigateNext/>
-        }
-    }
+        } //this does not assign state, so it does not need to be bound
+    } //we want this so the User has visual feedback as to which state they are in, to KNOW that the field is supposed to be open or not
 
-    makeCiv(e){
+    makeCiv(e){ //on button click, make a call to the backend to get a civilization
         axios.get(callSetting+"newCiv/"+e.target.value).then(response=> {
-            let civ = response.data;
-            let setciv = this.state.civilization.concat(civ);
-            this.setState({'civilization': setciv});
-        }).catch(error => {
+            let civ = response.data; //temp value which holds a Civilization object from the backend
+            let setciv = this.state.civilization.concat(civ); //add the Civilization to the array of civilizations held in state, assign it a value
+            this.setState({'civilization': setciv}); //assign the new state, which now has the added civ, to the total state
+        }).catch(error => { //log error as needed
             errorLogger(error);});
     }
 
-    loadData(key){
-        if(this.state.civilization != null){
-        let holdme = Object.keys(this.state.civilization).map(x =>
-            <Civilization key={x} number={x} civilization ={this.state.civilization[x]} />
+    loadData(key){ //load data from the state
+        if(this.state.civilization != null){ //check for civlization, if there isn't one, don't do anything
+        let holdme = Object.keys(this.state.civilization).map(x => // there is a civ in state, get the keys of that Array and map those objects to make a number of civilization components
+            <Civilization key={x} number={x} civilization ={this.state.civilization[x]} /> //we must pass in key (to suppress warnings), number (count of a given Civ object in an array), and the civ object itself
             );
-        return holdme;
+        return holdme; //that list of Civilization components is added to the holdMe object, we now pass that object which is loaded with JSX, it will populate the page
         }
+    }
+
+    handleCivNumber(event){
+        this.setState({civsInSetting: event.target.value});
     }
 
     render(){
@@ -99,9 +107,10 @@ export class Setting extends React.Component {
                 </div>
             </h3>
             <Collapse isOpen={this.state.civs}>
-                Blah blah blah
-                <Button id="makeCiv" value="1" onClick={this.makeCiv}>Make Something</Button>
-                <ul>{this.loadData("civilization")}</ul>
+                <p style={{display:"inline"}}>Number of Civs in your Setting:</p><input type="number" value={this.state.civsInSetting} onChange={this.handleCivNumber}></input>
+                <br/>
+                <Button id="makeCiv" value={this.state.civsInSetting} onClick={this.makeCiv} /*Need to make a way to set value soon*/>Make Something</Button>
+                {this.loadData("civilization")}
                 
             </Collapse>
 
